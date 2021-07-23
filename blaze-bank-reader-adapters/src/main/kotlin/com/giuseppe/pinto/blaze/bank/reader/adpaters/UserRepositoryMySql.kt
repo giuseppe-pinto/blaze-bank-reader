477dd32cc3d0183e71f98fc355564250c21fc7f4
Giuseppe Pinto <giuseppe.pinto@lastminute.com>
2021-07-23 17:15:46 +0200

    wip - removed DAO, using directly repository and added testContainers

--- blaze-bank-reader-adapters/src/main/kotlin/com/giuseppe/pinto/blaze/bank/reader/adpaters/UserRepositoryMySql.kt
+++ blaze-bank-reader-adapters/src/main/kotlin/com/giuseppe/pinto/blaze/bank/reader/adpaters/UserRepositoryMySql.kt
@@ -2,23 +2,17 @@ package com.giuseppe.pinto.blaze.bank.reader.adpaters
 
 import com.giuseppe.pinto.blaze.bank.reader.domain.User
 import com.giuseppe.pinto.blaze.bank.reader.domain.UserRepository
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
 
-class UserRepositoryMySql(val dao: UserRepositoryDao) : UserRepository {
+class UserRepositoryMySql(private val jdbcTemplate: JdbcTemplate) : UserRepository {
 
     override fun getUserInfo(id: Long): User {
-        return convert(dao.getUser(id))
+        return jdbcTemplate.query("SELECT * FROM USER WHERE ID=$id", rawMapper()).first()
     }
 
-    private fun convert(user: UserDto): User {
-        return User(
-            user.id,
-            user.name,
-            user.lastname,
-            user.email,
-            user.phone,
-            user.address,
-            user.city,
-            user.country
-        )
+    private fun rawMapper(): RowMapper<User> {
+        TODO("Not yet implemented")
     }
+
 }
--- blaze-bank-reader-adapters/src/test/kotlin/com/giuseppe/pinto/blaze/bank/reader/adpaters/UserRepositoryMySqlTest.kt
+++ blaze-bank-reader-adapters/src/test/kotlin/com/giuseppe/pinto/blaze/bank/reader/adpaters/UserRepositoryMySqlTest.kt
@@ -1,29 +1,65 @@
 package com.giuseppe.pinto.blaze.bank.reader.adpaters
 
 import com.giuseppe.pinto.blaze.bank.reader.domain.User
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.datasource.DataSourceTransactionManager
+import org.springframework.transaction.support.DefaultTransactionDefinition
+import org.testcontainers.containers.MySQLContainer
+import javax.sql.DataSource
+
 
 class UserRepositoryMySqlTest {
+
+    lateinit var mysql: MySQLContainer<Nothing>
+    lateinit var dataSource: DataSource
+
+    @BeforeEach
+    internal fun setUp() {
+        mysql = MySQLContainer<Nothing>("mysql:5.5.53")
+        mysql.start()
+        dataSource = DataSourceBuilder.create()
+            .url(mysql.jdbcUrl)
+            .username(mysql.username)
+            .password(mysql.password)
+            .driverClassName("com.mysql.cj.jdbc.Driver")
+            .build()
+    }
+
+    @AfterEach
+    internal fun tearDown() {
+        mysql.close()
+    }
 
     @Test
     internal fun `get the user info`() {
 
-        val dao : UserRepositoryDao = mock()
+        val txManager = DataSourceTransactionManager(dataSource)
+        val jdbcTemplate = JdbcTemplate(dataSource)
+        val transaction = txManager.getTransaction(DefaultTransactionDefinition())
+        jdbcTemplate.execute(
+            "" +
+                    "CREATE TABLE `USER` (" +
+                    "  `ID` bigint NOT NULL AUTO_INCREMENT," +
+                    "  `NAME` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL," +
+                    "  `LASTNAME` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL," +
+                    "  `EMAIL` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL," +
+                    "  `PHONE` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL," +
+                    "  `ADDRESS` varchar(500) COLLATE utf8mb4_unicode_ci DEFAULT NULL," +
+                    "  `COUNTRY` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL," +
+                    "  `CITY` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL," +
+                    "  PRIMARY KEY (`ID`)," +
+                    "  UNIQUE KEY `USER_UN` (`NAME`,`LASTNAME`,`EMAIL`)," +
+                    "  KEY `USER_ID_IDX` (`ID`) USING BTREE" +
+                    ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;"
+        )
+        txManager.commit(transaction)
 
-        whenever(dao.getUser(1L)).thenReturn(
-            UserDto(1L,
-                "giuseppe",
-                "Pinto",
-                "cicciopasticcio@gmail.com",
-                "8883434567",
-                "via da via anfossi 37",
-                "Milan",
-                "IT"))
-
-        val sut = UserRepositoryMySql(dao)
+        val sut = UserRepositoryMySql(JdbcTemplate(dataSource))
 
         val expectedUser = User(
             1L,
